7de8be724d0c9513116fc199d64b28415de5ed69
venkataharish341 <vharishtavva@gmail.com>
2020-05-30 01:34:55 -0500

    Added Jasypt for encrypting/decrypting passwords.

--- OAuth2-Password-Flow/todo/src/main/java/com/learn2earn/todo/TodoApplication.java
+++ OAuth2-Password-Flow/todo/src/main/java/com/learn2earn/todo/TodoApplication.java
@@ -8,8 +8,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import com.learn2earn.todo.services.EmailService;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties // Application understands there are encrypted passwords.
 @EnableResourceServer // This makes it resource server
 @EnableGlobalMethodSecurity(prePostEnabled=true) // This enables to add security at method level. 
 public class TodoApplication implements CommandLineRunner {
